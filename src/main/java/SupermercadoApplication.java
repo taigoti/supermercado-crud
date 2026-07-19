59920e43ebb80165af64ed347fe601ba2d555059
taigoti <taigoti@proton.me>
2026-07-19 12:39:27 -0300

    feat: menu de escolha de operações CRUD

--- src/main/java/SupermercadoApplication.java
+++ src/main/java/SupermercadoApplication.java
@@ -1,5 +1,39 @@
+import dao.ProdutoDAO;
+import model.Produto;
+import java.util.List;
+import java.util.Scanner;
+
 public class SupermercadoApplication {
-    static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        ProdutoDAO dao = new ProdutoDAO();
+        escolherOperacao();
     }
+
+    static void escolherOperacao() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+                1: Inserir | 2: Buscar por Id | 3: Buscar todos | 4: Atualizar | 5: Deletar
+                Digite a operação que quer realizar:""");
+
+        int op = sc.nextInt();
+
+        switch (op) {
+            case 1 -> inserir();
+            case 2 -> buscar();
+            case 3 -> buscarTodos();
+            case 4 -> atualizar();
+            case 5 -> deletar();
+            default -> {
+                System.out.println("Digite uma operação válida!");
+                escolherOperacao();
+            }
+        }
+    }
+
+    static void inserir() {}
+    static void buscar() {}
+    static void buscarTodos() {}
+    static void atualizar() {}
+    static void deletar() {}
 }
